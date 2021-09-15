f2786fc262e9a52c0686bfc0d9987b8c480fdd26
Kevin <kevinator900@gmail.com>
2021-09-15 13:28:30 -0700

    finished the styling

--- React/09-hacker-news/src/Components/Pagination.jsx
+++ React/09-hacker-news/src/Components/Pagination.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNewsContext } from "../util/context";
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
 
 const Pagination = () => {
   const { page, setPage, news, newPage, setNewPage } = useNewsContext();
@@ -8,6 +8,7 @@ const Pagination = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if(!Number(newPage)) {
+      setNewPage(page + 1)
       return;
     }
     if (newPage <= 0) {
@@ -22,17 +23,32 @@ const Pagination = () => {
   };
 
   const handleClick = (dir) => {
-    if(dir === 'left'){
-      setPage(page <= 0 ? news.nbPages - 1 : page - 1)
+    switch (dir){
+      case 'left':
+        setNewPage(page <= 0 ? news.nbPages : page )
+        setPage(page <= 0 ? news.nbPages - 1 : page - 1)
+        break;
+      case 'right':
+        setNewPage(page + 1 >= news.nbPages ? 1 : page + 2)
+        setPage(page + 1 >= news.nbPages ? 0 : page + 1)
+        break;
+      case 'hard-right':
+        setNewPage(news.nbPages)
+        setPage(news.nbPages - 1)
+        break;
+      case 'hard-left':
+        setNewPage(1)
+        setPage(0)
+        break;
     }
-    if(dir === 'right'){
-      setPage(page + 1 >= news.nbPages ? 0 : page + 1)
-    }
-    setNewPage(page + 1)
   }
 
   return (
     <form onSubmit={handleSubmit} className="pagination">
+      <AiOutlineDoubleLeft
+        className="arrow"
+        onClick={() => {handleClick('hard-left')}}
+        />
       <AiOutlineArrowLeft
         className="arrow"
         onClick={() => {handleClick('left')}}
@@ -50,6 +66,10 @@ const Pagination = () => {
         className="arrow"
         onClick={() => {handleClick('right')}}
       />
+      <AiOutlineDoubleRight
+        className="arrow"
+        onClick={() => {handleClick('hard-right')}}
+        />
     </form>
   );
 };
